d1dbb81c638be71db8d7d82f62799ba276d66ba4
Parthasai <53685926+propardhu@users.noreply.github.com>
2022-07-07 21:05:31 +0530

    user

---
+++ src/main/java/com/pardhu/authdemo/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.pardhu.authdemo.repository;
+
+import com.pardhu.authdemo.domain.Authority;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {}
--- src/main/java/com/pardhu/authdemo/repository/UserRepository.java
+++ src/main/java/com/pardhu/authdemo/repository/UserRepository.java
@@ -1,11 +1,28 @@
 package com.pardhu.authdemo.repository;
 
 import com.pardhu.authdemo.domain.User;
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Spring Data MongoDB repository for the {@link User} entity.
+ */
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-  Optional<User> findByUsername(String username);
+    Optional<User> findOneByActivationKey(String activationKey);
+
+    List<User> findAllByActivatedIsFalseAndActivationKeyIsNotNullAndCreatedDateBefore(Instant dateTime);
+
+    Optional<User> findOneByResetKey(String resetKey);
+
+    Optional<User> findOneByEmailIgnoreCase(String email);
+
+    Optional<User> findOneByLogin(String login);
+
+    Page<User> findAllByIdNotNullAndActivatedIsTrue(Pageable pageable);
 }
